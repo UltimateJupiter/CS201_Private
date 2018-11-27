f96093a1ea145eacba3b6bf2972f9ae60f89f6af
JupiterZhu <jupiter_zhu_007@126.com>
2018-11-27 01:16:28 -0500

    Uploading Codes for APT7 (Circuit.java)

--- APT7/src/Circuits.java
+++ APT7/src/Circuits.java
@@ -2,26 +2,26 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Circuits {
-	
-	ArrayList<ArrayList<Integer>> con = new ArrayList<ArrayList<Integer>>();
+    ArrayList<ArrayList<Integer>> con = new ArrayList<ArrayList<Integer>>();
     ArrayList<ArrayList<Integer>> cos = new ArrayList<ArrayList<Integer>>();
-	ArrayList<Integer> total = new ArrayList<Integer>();
-	
-	public void calc_remain(int node, int cost) {
-		if (con.get(node).size() == 0) total.add(cost);
-		for (int k = 0; k < con.get(node).size(); k++) {
-			int nd = con.get(node).get(k);
-			calc_remain(nd, cost + cos.get(node).get(k));
-		}
-	}
+    ArrayList<Integer> total = new ArrayList<Integer>();
+    
+    public void calc_remain(int node, int cost) {
+        
+        if (con.get(node).size() == 0) total.add(cost);
+        for (int k = 0; k < con.get(node).size(); k++) {
+            int nd = con.get(node).get(k);
+            calc_remain(nd, cost + cos.get(node).get(k));
+        }
+    }
     public int howLong(String[] connects, String[] costs) {
         
         for (int i = 0; i < connects.length; i++) {
-        	con.add(new ArrayList<Integer>());
-        	cos.add(new ArrayList<Integer>());
-        	if (connects[i].equals("")) continue;
-        	for (String str: connects[i].split("\\s+")) con.get(i).add(Integer.parseInt(str));
-        	for (String str: costs[i].split("\\s+")) cos.get(i).add(Integer.parseInt(str));
+            con.add(new ArrayList<Integer>());
+            cos.add(new ArrayList<Integer>());
+            if (connects[i].equals("")) continue;
+            for (String str: connects[i].split("\\s+")) con.get(i).add(Integer.parseInt(str));
+            for (String str: costs[i].split("\\s+")) cos.get(i).add(Integer.parseInt(str));
         }
         
         for (int i = 0; i < connects.length; i++) calc_remain(i, 0);
